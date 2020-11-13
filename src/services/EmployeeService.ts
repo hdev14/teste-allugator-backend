0af8411e4a1235696b94359b12e59d38091aa2c9
Hermerson Araújo <ne.hermerson@gmail.com>
2020-11-13 09:33:32 -0300

    feat: ensure EmployeeService return a list of employees by register date

--- src/services/EmployeeService.ts
+++ src/services/EmployeeService.ts
@@ -20,6 +20,12 @@ class EmployeeService {
     const employees = await employeesCollection.find({ cargo: roleFilterRegExp }).toArray()
     return employees
   }
+
+  public async getEmployeesByRegisterDate (date: string) {
+    const employeesCollection = Mongo.getCollection('employees')
+    const employees = await employeesCollection.find({ datacad: date }).toArray()
+    return employees
+  }
 }
 
 export default EmployeeService
