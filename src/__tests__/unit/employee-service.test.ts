a1b1127b595c03ef7661cc28a7e652cf7e406200
Hermerson Araújo <ne.hermerson@gmail.com>
2020-11-13 09:08:48 -0300

    test: create test to EmployeeService.getEmployeeByCPF

--- src/__tests__/unit/employee-service.test.ts
+++ src/__tests__/unit/employee-service.test.ts
@@ -11,7 +11,7 @@ describe('EmployeeService Unit Tests', () => {
       {
         DataCad: '15/04/2017',
         Cargo: 'Dev Jr',
-        Cpf: 85235708709,
+        Cpf: '85235708709',
         Nome: 'Aaron Aaberg',
         UfNasc: 'AP',
         Salario: 8965.30,
@@ -20,7 +20,7 @@ describe('EmployeeService Unit Tests', () => {
       {
         DataCad: '19/04/2017',
         Cargo: 'AC Sr',
-        Cpf: 59984408701,
+        Cpf: '59984408701',
         Nome: 'Aaron Aaby',
         UfNasc: 'RO',
         Salario: 5312.70,
@@ -29,7 +29,7 @@ describe('EmployeeService Unit Tests', () => {
       {
         DataCad: '19/04/2017',
         Cargo: 'PO Jr',
-        Cpf: 32439637882,
+        Cpf: '32439637882',
         Nome: 'Abbie Aagaard',
         UfNasc: 'PR',
         Salario: 3655.10,
@@ -54,5 +54,18 @@ describe('EmployeeService Unit Tests', () => {
 
     const employeesWithFirstNameAbbie = await employeeService.getEmployeesByName('Abbie')
     expect(employeesWithFirstNameAbbie.length).toBe(1)
+  })
+
+  it('should return an employee by name', async () => {
+    const employeeService = new EmployeeService()
+
+    let employee = await employeeService.getEmployeesByCPF('85235708709')
+    expect(employee.Nome).toBe('Aaron Aaberg')
+
+    employee = await employeeService.getEmployeesByCPF('59984408701')
+    expect(employee.Nome).toBe('Aaron Aaby')
+
+    employee = await employeeService.getEmployeesByCPF('32439637882')
+    expect(employee.Nome).toBe('Abbie Aagaard')
   })
 })
